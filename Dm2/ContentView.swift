f007fc4fde7ae1208391be6cf30fd109f1329af3
TheRealSalvo <salva0729@gmail.com>
2024-05-07 21:26:55 +0200

    first commit

---
+++ Dm2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dm2
+//
+//  Created by Salvatore Attanasio on 07/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dm2/Dm2App.swift
@@ -0,0 +1,17 @@
+//
+//  Dm2App.swift
+//  Dm2
+//
+//  Created by Salvatore Attanasio on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Dm2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
