a46d6a51c2b17ee405c0a8abb42fcc1e559acf67
TheRealSalvo <salva0729@gmail.com>
2024-05-18 17:56:29 +0200

    added models for managing routes

---
+++ Dm2/Models/Route.swift
@@ -0,0 +1,13 @@
+//
+//  Route.swift
+//  Dm2
+//
+//  Created by Salvatore Attanasio on 18/05/24.
+//
+
+import Foundation
+
+struct Route{
+    let destination : String
+    let waypoints : [Waypoint]
+}
